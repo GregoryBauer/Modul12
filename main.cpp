f5dfa39042739f78b25e17d5f07ed85c33c8e3e8
Greg Farakhutdinov <125730666+GregoryBauer@users.noreply.github.com>
2023-04-22 11:29:04 +0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <string.h>
+
+int IsKPeriodic(const std::string& str, int K)
+{
+	for (int i = 0; str.length() - K; i++)
+	{
+		if (str[i] == str[i + K])
+		{
+			std::cout << "Строка кратна К" << std::endl;
+			return true;
+		}
+		else {
+			std::cout << "Строка не кратна К" << std::endl;
+			return false;
+		}
+	}
+}
+
+int main()
+{
+	setlocale(LC_ALL, "");
+	std::string str;
+
+	int K;
+
+	std::cout << "Введите вашу строку" << std::endl;
+
+	std::cin >> str;
+
+	std::cout << "Введите К для проверки строки" << std::endl;
+
+	std::cin >> K;
+
+	IsKPeriodic(str, K); // Вывод, кратна строка или не кратна.
+	
+	return 0;
+}
